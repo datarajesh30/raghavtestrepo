2351661e3ddd1aeeb2a4ecda622e975800337e98
datarajesh30 <datarajesh30@gmail.com>
2026-06-29 23:19:12 +0530

    rg-code added

--- main.tf
+++ main.tf
@@ -1,1 +1,23 @@
 # this is the main.tf file 
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = "<YOUR_SUBSCRIPTION_ID>"
+}
+
+resource "azurerm_resource_group" "rajesh_rg" {
+  name     = "rajesh-rg"
+  location = "Central India"
+
+}
